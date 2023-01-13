6106be1e9804510f0fe7c819f52db42d775def85
ali <mommy@pleasedrinkwater.local>
2023-01-13 09:11:39 +0900

    have jsx in state, need to fix

--- src/components/OptionContainer.js
+++ src/components/OptionContainer.js
@@ -4,12 +4,23 @@ import OptionInput from './OptionInput';
 export default function OptionContainer({initAmount, className}){
     let itemList=[];
     for (let i =0 ; i< initAmount; i++) {
-        itemList.push(<OptionInput key={className + i}/>)
+        itemList.push(
+            <div key={className + i}>
+                <OptionInput/>
+                <button onClick={handleDelete(className+i)}>-</button>
+            </div>
+        )
     }
     const [elements, setElements] = useState(itemList);
 
     function handleAdd() {
         setElements([...elements,<OptionInput id={className + elements.length+1}/>])
+    }
+    function handleDelete(key){
+        setElements(oldValues => {
+            return oldValues.filter(input => input.key !== key)
+        })
+        console.log(key)
     }
 
     return(
