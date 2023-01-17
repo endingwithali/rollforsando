4d36c35baa16dda4022dcab1aa597a0ff6f0b40b
ali <mommy@pleasedrinkwater.local>
2023-01-17 12:25:29 +0900

    now have child option component takign in parent state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
 import OptionContainer from './components/OptionContainer';
+import React, {useState} from 'react';
+import { isCompositeComponent } from 'react-dom/test-utils';
+
 
 function App() {
-
+  const [parts, setParts] = useState([{
+    id: "bread",
+    initAmount: 6,
+    values: []
+  }, {
+    id: "bread",
+    initAmount: 6,
+    values: []
+  }]);
+  console.log(parts)
 
   function handleRoll(){
+    let sandwich = []
+    parts.forEach((part)=>{
+      console.log(part.value.length)
+      console.log(part)
+      sandwich.push(part.values[Math.floor((Math.random() * (part.values.length)))])
+    })
+    console.log(sandwich)
     console.log("rolling")
+    
+
+    /**
+     * for each optioncontainer
+     * get lenght of number of children elements where child == input
+     * get values
+     * rng a number based on those values
+     * get the value mapped to the rng number
+     * store value for later in array of selected valus
+     */
   }
+
+  /* main = the meat 
+  rougage = 2x d12
+  wildmagic = random tings
+
+      <OptionContainer initAmount={6} className="bread"/>
+      <OptionContainer initAmount={12} className="main"/> 
+      <OptionContainer initAmount={6} className="cheese"/> 
+      <OptionContainer initAmount={12} className="roughage"/> 
+      <OptionContainer initAmount={20} className="wildmagic"/> 
+      <OptionContainer initAmount={20} className="sauce"/> 
+
+
+  */
+
   return (
     <div className="App">
       {/* <header className="App-header">
       </header> */}
-      <OptionContainer initAmount={6} className="bread"/>
-      <OptionContainer initAmount={10} className="lettuce"/>
-
+      <div id={"lists"}>
+            {parts.map((item, index) =>
+              <OptionContainer parentIndex={index} parentState={parts}></OptionContainer>
+            )}
+      </div>
+  
+      {/* <OptionContainer initAmount={6} className="bread" state={parts[0]}/> */}
+      {/* <OptionContainer initAmount={6} className="main"/>  */}
       <button onClick={() => handleRoll()}>Roll for Sandwich</button>
     </div>
   );
 }
 
 export default App;
+
+// const [sandwich, setSandwich] = useState[{heading: 'bread', values:[]}]
+// return (
+//   div 
+//   ((v)=>OptionContainer val=v setState={setSandwich})
+// )
+
+// state = [{
+//   id: "bread",
+//   selectedValue: ""
+// }, {
+//   id: "roughage",
+//   selectedValue: ""
+// }, {
+//   id: "main",
+//   selectedValue: ""
+// }]
+
+// in OptionContainer
+
+// elements,setelements = usestate(state(passed prop))
+
+
+
+// input onchange =>
+// elements.get(passedprop id)[index] = {id:elements[index].id,value:value}
+// setElements (elements)
+
+
+// in App
+// roll onclick =>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/components/OptionContainer.js
+++ src/components/OptionContainer.js
@@ -8,21 +8,29 @@ function generateArray(lenght){
     return initArray
 }
 
-export default function OptionContainer({initAmount, className}){
-
+export default function OptionContainer({parentIndex, parentState}){
+    const [parentElements, setParentState] = useState(parentState)
+    const initAmount = parentElements[parentIndex].initAmount
+    const className = parentElements[parentIndex].id
     const [elements, setElements] = useState(generateArray(initAmount));
 
     function handleAdd() {
         setElements([...elements, {id: (Math . random() + 1). toString(36).substring(2),value:""}])
+        parentElements[parentIndex].value = elements
+        setParentState(parentElements)
     }
 
     function handleDelete(id){
         setElements(elements => elements.filter((element) => element.id !== id ))
+        parentElements[parentIndex].value = elements
+        setParentState(parentElements)
     }
 
     function handleChange(index, value){
         elements[index]={id:elements[index].id,value:value}
         setElements(elements)
+        parentElements[parentIndex].value = elements
+        setParentState(parentElements)
     }
 
     return(
